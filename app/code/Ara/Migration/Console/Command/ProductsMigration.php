8dabd2cd339dee8450c3fecae694faea862f3d0b
a.amatuni <a.amatuni@racechip.com>
2016-09-28 17:49:29 +0200

    Fix image exception

--- app/code/Ara/Migration/Console/Command/ProductsMigration.php
+++ app/code/Ara/Migration/Console/Command/ProductsMigration.php
@@ -5,6 +5,7 @@
  */
 namespace Ara\Migration\Console\Command;
 
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -130,12 +131,16 @@ class ProductsMigration extends \Symfony\Component\Console\Command\Command
 
 
             if (!empty($item['image'])) {
-                $product->addImageToMediaGallery(
-                    'tmp/catalog/product/' . $item['image'],
-                    ['thumbnail', 'small_image', 'image'],
-                    false,
-                    false
-                );
+                try {
+                    $product->addImageToMediaGallery(
+                        'tmp/catalog/product/' . $item['image'],
+                        ['thumbnail', 'small_image', 'image'],
+                        false,
+                        false
+                    );
+                } catch (Exception $e) {
+                    echo $e->getMessage();
+                }
             }
             $product->save();
         }
